38aa7ab0608805c0a22a133bb0efa732ff0c1837
Luciara-Abreu <luciara.abreu@gmail.com>
2024-04-07 20:06:33 -0300

    add exemple data

---
+++ data.js
@@ -0,0 +1,79 @@
+//base exemplo para tabela livros
+[
+  {
+    title: 'Saturnino desce ao pampa',
+    author: 'luis Coronel',
+    yearPublication: '2020-01-15T03:00:00.000Z',
+    genre: 'fantasia - infantil ',
+    cover: 'url...',
+    quantityPages: 20,
+    quantityBook: 3,
+    booksInStock: 3,
+  },
+  {
+    title: 'A Grande Orquestra dos Bichos',
+    author: 'luis Coronel',
+    yearPublication: '2021-01-15T03:00:00.000Z',
+    genre: 'fantasia - infantil ',
+    cover: 'url...',
+    quantityPages: 20,
+    quantityBook: 3,
+    booksInStock: 3,
+  },
+  {
+    title: 'Memórias e Estórias do Visconde de Aceguá',
+    author: 'luis Coronel',
+    yearPublication: '2022-03-25T03:00:00.000Z',
+    genre: 'fantasia - infantil ',
+    cover: 'url...',
+    quantityPages: 20,
+    quantityBook: 3,
+    booksInStock: 3,
+  },
+  {
+    title: 'A Eleição dos animais',
+    author: 'luis Coronel',
+    yearPublication: '2019-07-12T03:00:00.000Z',
+    genre: 'fantasia - infantil ',
+    cover: 'url...',
+    quantityPages: 20,
+    quantityBook: 3,
+    booksInStock: 3,
+  },
+  {
+    title: 'Um canto de amor a natureza',
+    author: 'luis Coronel',
+    yearPublication: '2024-09-22T03:00:00.000Z',
+    genre: 'fantasia - infantil ',
+    cover: 'url...',
+    quantityPages: 20,
+    quantityBook: 3,
+    booksInStock: 3,
+  },
+];
+
+//base exemplo users
+
+[
+  {
+    name: 'Eli Izabel',
+    email: 'eli.i@gmail.com',
+    fone: '51 777888520',
+    role: 2,
+    password: '123456',
+  },
+  {
+    name: 'Anna Maria',
+    email: 'anna.m@gmail.com',
+    fone: '51 777888520',
+    role: 1,
+    password: '123456',
+  },
+  {
+    name: 'Luci Abreu',
+    email: 'luci.a@gmail.com',
+    fone: '51 777888520',
+    role: 2,
+    password: '123456',
+  },
+];
