158db9ed5ff9e3d2fc5848041bcd7a12cb1c5d9e
Luci Abreu <luciara.abreu@gmail.com>
2024-04-10 18:38:14 -0300

    refactor add controller

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,63 +1,32 @@
 import { Request, Response, Router } from 'express';
 import { BookService } from '../service/book.service';
 import { BookRepository } from '../repository/book.repopsitory';
+import { BookController } from '../controllers/book.controller';
 
 const route = Router();
 
 const bookRepository = new BookRepository();
 const bookService = new BookService(bookRepository);
+const bookController = new BookController(bookService);
 
-//listar todos
 route.get('/', async (req: Request, res: Response) => {
-  try {
-    const listBooks = await bookService.getAll();
-    res.status(200).send(listBooks);
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
-  }
+  return bookController.getAll(req, res);
 });
 
-//listar um
 route.get('/:id', async (req: Request, res: Response) => {
-  const id = req.params.id;
-  try {
-    const book = await bookService.getOne(id);
-    res.status(200).send(book);
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
-  }
+  return bookController.getId(req, res);
 });
 
-//add book
 route.post('/', async (req: Request, res: Response) => {
-  try {
-    await bookService.create(req.body.title, req.body.author, req.body);
-    res.status(200).send({ message: 'Livro adicionado com sucesso!' });
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
-  }
+  return bookController.create(req, res);
 });
 
-//atualizar
 route.patch('/:id', async (req: Request, res: Response) => {
-  const id = req.params.id;
-  try {
-    await bookService.update(id, req.body);
-    res.status(200).send({ message: 'Livro atualizado com sucesso' });
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
-  }
+  return bookController.update(req, res);
 });
 
-//deletar
 route.delete('/:id', async (req: Request, res: Response) => {
-  const id = req.params.id;
-  try {
-    await bookService.remove(id);
-    res.status(200).send({ message: 'Livro deletado com sucesso!' });
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
-  }
+  return bookController.update(req, res);
 });
 
 export default route;
