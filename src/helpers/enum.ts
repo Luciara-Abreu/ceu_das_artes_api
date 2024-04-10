ee16d8b9999d038fa304091079115debd581a091
Luci Abreu <luciara.abreu@gmail.com>
2024-04-10 12:42:50 -0300

    feat / add course

--- src/helpers/enum.ts
+++ src/helpers/enum.ts
@@ -1,4 +1,15 @@
 export enum Role {
   User = 1,
   Admin = 2,
+  instructor = 3,
+}
+
+export enum DayWeek {
+  Domingo = 0,
+  Segunda = 1,
+  Terca = 2,
+  Quarta = 3,
+  Quinta = 4,
+  Sexta = 5,
+  Sabado = 6,
 }
