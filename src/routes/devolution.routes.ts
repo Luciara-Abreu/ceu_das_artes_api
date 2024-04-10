cb8f9b4fc2562b21f06e9363ec0717ba5c3af9e8
Luci Abreu <luciara.abreu@gmail.com>
2024-04-10 19:07:41 -0300

    refactor add controller

--- src/routes/devolution.routes.ts
+++ src/routes/devolution.routes.ts
@@ -3,6 +3,7 @@ import { DevolutionService } from '../service/devolution.service';
 import { DevolutionRepository } from '../repository/devolution.repository';
 import { BookRepository } from '../repository/book.repopsitory';
 import { LocationRepository } from '../repository/location.repopsitory';
+import { DevolutionController } from '../controllers/devolution.controller';
 
 const route = Router();
 
@@ -10,58 +11,26 @@ const devolutionRepository = new DevolutionRepository();
 const bookRepository = new BookRepository();
 const locationRepopsitory = new LocationRepository();
 const devolutionService = new DevolutionService(devolutionRepository, bookRepository, locationRepopsitory);
+const devolutionController = new DevolutionController(devolutionService);
 
-//listar todos
 route.get('/', async (req: Request, res: Response) => {
-  try {
-    const devolutions = await devolutionService.getAll();
-    res.status(200).send(devolutions);
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
-  }
+  return devolutionController.getAll(req, res);
 });
 
-//listar um
 route.get('/:id', async (req: Request, res: Response) => {
-  const id = req.params.id;
-  try {
-    const devolution = await devolutionService.getOne(id);
-    res.status(200).send(devolution);
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
-  }
+  return devolutionController.getId(req, res);
 });
 
-//add devolution
 route.post('/', async (req: Request, res: Response) => {
-  try {
-    await devolutionService.create(req.body.bookId, req.body.userId, req.body);
-    res.status(200).send({ message: 'Devolução efetuada com sucesso!' });
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
-  }
+  return devolutionController.create(req, res);
 });
 
-//atualizar
 route.patch('/:id', async (req: Request, res: Response) => {
-  const id = req.params.id;
-  try {
-    await devolutionService.update(id, req.body);
-    res.status(200).send({ message: 'Devolução atualizado com sucesso' });
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
-  }
+  return devolutionController.update(req, res);
 });
 
-//deletar
 route.delete('/:id', async (req: Request, res: Response) => {
-  const id = req.params.id;
-  try {
-    await devolutionService.remove(id);
-    res.status(200).send({ message: 'Devolução deletado com sucesso!' });
-  } catch (error: any) {
-    res.status(400).send({ message: error.message });
-  }
+  return devolutionController.delete(req, res);
 });
 
 export default route;
