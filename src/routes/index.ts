7e5f25b546df027b6ecb74b375de87c1af25d969
Luci Abreu <luciara.abreu@gmail.com>
2024-04-10 18:00:42 -0300

    correction

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,6 @@ import userRoute from './user.routes';
 import bookRoute from './book.routes';
 import locationRoute from './location.routes';
 import devolutionRoute from './devolution.routes';
-<<<<<<< HEAD
-import courseRoute from './course.routes';
-
-=======
->>>>>>> 6a4ab4b (Add user authentication)
 import courseRoute from './course.routes';
 
 const router = Router();
