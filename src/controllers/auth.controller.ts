af6ded1bbacb30e4eb8f697e547ed66d5e9f070e
Luis Barufi <luisbarufi@gmail.com>
2024-04-11 19:38:21 -0300

    Adds password recovery features

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -17,4 +17,24 @@ export class AuthController {
   async profile(req: Request, res: Response) {
     return res.status(200).json(req.user);
   }
+
+  async forget(req: Request, res: Response) {
+    try {
+      await this.authService.forget(req.body.email);
+
+      return res.status(200).send({ message: 'E-mail enviado com sucesso!' });
+    } catch (error) {
+      res.status(400).send({ message: error.message });
+    }
+  }
+
+  async reset(req: Request, res: Response) {
+    try {
+      const user = await this.authService.reset(req.body.password, req.body.token);
+
+      return res.status(200).json(user);
+    } catch (error: any) {
+      res.status(400).send({ message: error.message });
+    }
+  }
 }
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,17 +1,25 @@
 import { Request, Response, Router } from 'express';
 import { AuthService } from '../service/auth.service';
-import { AuthRepository } from '../repository/auth.repository';
 import { AuthController } from '../controllers/auth.controller';
 import { authMiddleware } from '../middlewares/auth.middleware';
+import { EtherealMailProvider } from '../providers/ethereal-mail.provider';
 
 const route = Router();
 
-const authRepository = new AuthRepository();
-const authService = new AuthService(authRepository);
+const etherealMailProvider = new EtherealMailProvider();
+const authService = new AuthService(etherealMailProvider);
 const authController = new AuthController(authService);
 
 route.post('/', async (req: Request, res: Response) => {
   return authController.login(req, res);
+});
+
+route.post('/forget', async (req: Request, res: Response) => {
+  return authController.forget(req, res);
+});
+
+route.post('/reset', async (req: Request, res: Response) => {
+  return authController.reset(req, res);
 });
 
 route.get('/profile', authMiddleware, async (req: Request, res: Response) => {
