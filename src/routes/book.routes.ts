4300a288e27a69fba6462d48cf3c1cf2ddb99f29
Luci Abreu <luciara.abreu@gmail.com>
2024-04-12 13:27:46 -0300

    code cleaning and refactoring

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,12 +1,10 @@
 import { Request, Response, Router } from 'express';
 import { BookService } from '../service/book.service';
-import { BookRepository } from '../repository/book.repopsitory';
 import { BookController } from '../controllers/book.controller';
 
 const route = Router();
 
-const bookRepository = new BookRepository();
-const bookService = new BookService(bookRepository);
+const bookService = new BookService();
 const bookController = new BookController(bookService);
 
 route.get('/', async (req: Request, res: Response) => {
