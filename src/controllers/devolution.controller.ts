7088f1aead6baead62c4da3ee05a3e1a0e38a88e
Luci Abreu <luciara.abreu@gmail.com>
2024-04-13 19:12:56 -0300

    Implements error handling for devolutions

--- src/controllers/devolution.controller.ts
+++ src/controllers/devolution.controller.ts
@@ -26,33 +26,24 @@ export class DevolutionController {
 
   //add devolution
   async create(req: Request, res: Response) {
-    try {
-      await this.devolutionService.create(req.body.bookId, req.body.userId, req.body);
-      res.status(200).send({ message: 'Devolução efetuada com sucesso!' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.devolutionService.create(req.body.bookId, req.body.userId, req.body);
+
+    return res.status(200).send({ message: 'Devolução efetuada com sucesso!' });
   }
 
   //atualizar
   async update(req: Request, res: Response) {
     const id = req.params.id;
-    try {
-      await this.devolutionService.update(id, req.body);
-      res.status(200).send({ message: 'Devolução atualizado com sucesso' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.devolutionService.update(id, req.body);
+
+    return res.status(200).send({ message: 'Devolução atualizado com sucesso' });
   }
 
   //deletar
   async delete(req: Request, res: Response) {
     const id = req.params.id;
-    try {
-      await this.devolutionService.remove(id);
-      res.status(200).send({ message: 'Devolução deletado com sucesso!' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.devolutionService.remove(id);
+
+    return res.status(200).send({ message: 'Devolução deletado com sucesso!' });
   }
 }
