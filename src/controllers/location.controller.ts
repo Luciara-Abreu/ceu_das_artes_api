3a613fb3497002b1be979a79ef2fe5e80ef3a9e8
Luci Abreu <luciara.abreu@gmail.com>
2024-04-13 19:10:16 -0300

    Implements error handling for locations

--- src/controllers/location.controller.ts
+++ src/controllers/location.controller.ts
@@ -5,50 +5,35 @@ export class LocationController {
   constructor(private locationService: LocationService) {}
 
   async getAll(req: Request, res: Response) {
-    try {
-      const listLocations = await this.locationService.getAll();
-      res.status(200).send(listLocations);
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    const listLocations = await this.locationService.getAll();
+
+    return res.status(200).send(listLocations);
   }
 
   async getId(req: Request, res: Response) {
     const id = req.params.id;
-    try {
-      const location = await this.locationService.getOne(id);
-      res.status(200).send(location);
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    const location = await this.locationService.getOne(id);
+
+    return res.status(200).send(location);
   }
 
   async create(req: Request, res: Response) {
-    try {
-      await this.locationService.create(req.body.bookId, req.body.userId, req.body);
-      res.status(200).send({ message: 'Locação efetuada com sucesso!' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.locationService.create(req.body.bookId, req.body.userId, req.body);
+
+    return res.status(200).send({ message: 'Locação efetuada com sucesso!' });
   }
 
   async update(req: Request, res: Response) {
     const id = req.params.id;
-    try {
-      await this.locationService.update(id, req.body);
-      res.status(200).send({ message: 'Locação atualizado com sucesso' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.locationService.update(id, req.body);
+
+    return res.status(200).send({ message: 'Locação atualizado com sucesso' });
   }
 
   async delete(req: Request, res: Response) {
     const id = req.params.id;
-    try {
-      await this.locationService.remove(id);
-      res.status(200).send({ message: 'Locação deletado com sucesso!' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.locationService.remove(id);
+
+    return res.status(200).send({ message: 'Locação deletado com sucesso!' });
   }
 }
