5edeb3440e4694d854287eba3e85e4a526773941
Luci Abreu <luciara.abreu@gmail.com>
2024-04-13 19:16:38 -0300

    Implements error handling for books

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -24,31 +24,22 @@ export class BookController {
   }
 
   async create(req: Request, res: Response) {
-    try {
-      await this.bookService.create(req.body.title, req.body.author, req.body);
-      res.status(200).send({ message: 'Livro adicionado com sucesso!' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.bookService.create(req.body.title, req.body.author, req.body);
+
+    return res.status(200).send({ message: 'Livro adicionado com sucesso!' });
   }
 
   async update(req: Request, res: Response) {
     const id = req.params.id;
-    try {
-      await this.bookService.update(id, req.body);
-      res.status(200).send({ message: 'Livro atualizado com sucesso' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.bookService.update(id, req.body);
+
+    return res.status(200).send({ message: 'Livro atualizado com sucesso' });
   }
 
   async delete(req: Request, res: Response) {
     const id = req.params.id;
-    try {
-      await this.bookService.remove(id);
-      res.status(200).send({ message: 'Livro deletado com sucesso!' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.bookService.remove(id);
+
+    return res.status(200).send({ message: 'Livro deletado com sucesso!' });
   }
 }
