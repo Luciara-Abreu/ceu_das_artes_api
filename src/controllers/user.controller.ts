926c58480688b37d1c4e261c468afbb9b1435c0f
Luci Abreu <luciara.abreu@gmail.com>
2024-04-13 19:04:24 -0300

    Implements error handling for users

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -5,50 +5,35 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   async getAll(req: Request, res: Response) {
-    try {
-      const list = await this.userService.getAll();
-      res.status(200).send(list);
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    const list = await this.userService.getAll();
+
+    return res.status(200).send(list);
   }
 
   async getId(req: Request, res: Response) {
     const id = req.params.id;
-    try {
-      const user = await this.userService.getOne(id);
-      res.status(200).send(user);
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    const user = await this.userService.getOne(id);
+
+    return res.status(200).send(user);
   }
 
   async create(req: Request, res: Response) {
-    try {
-      await this.userService.create(req.body.email, req.body);
-      res.status(200).send({ message: 'Usuário adicionado com sucesso!' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.userService.create(req.body.email, req.body);
+
+    return res.status(200).send({ message: 'Usuário adicionado com sucesso!' });
   }
 
   async update(req: Request, res: Response) {
     const id = req.params.id;
-    try {
-      await this.userService.update(id, req.body);
-      res.status(200).send({ message: 'Usuário atualizado' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.userService.update(id, req.body);
+
+    return res.status(200).send({ message: 'Usuário atualizado' });
   }
 
   async delete(req: Request, res: Response) {
     const id = req.params.id;
-    try {
-      await this.userService.remove(id);
-      res.status(200).send({ message: 'Usuário deletado com sucesso!' });
-    } catch (error: any) {
-      res.status(400).send({ message: error.message });
-    }
+    await this.userService.remove(id);
+
+    return res.status(200).send({ message: 'Usuário deletado com sucesso!' });
   }
 }
