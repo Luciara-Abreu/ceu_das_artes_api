a76b9390556ba9a63c261da49f0a5e29b4f75f50
Luis Barufi <luisbarufi@gmail.com>
2024-04-19 23:27:57 -0300

    Adds Mocks

---
+++ src/helpers/mocks/user/user.service.mock.ts
@@ -0,0 +1,9 @@
+import { userListMock } from './user.list.mock';
+
+export const UserServiceMock = {
+  list: jest.fn().mockResolvedValue(userListMock),
+  create: jest.fn().mockResolvedValue(userListMock[0]),
+  show: jest.fn().mockResolvedValue(userListMock[0]),
+  update: jest.fn().mockResolvedValue(userListMock[0]),
+  delete: jest.fn().mockResolvedValue(true),
+}
