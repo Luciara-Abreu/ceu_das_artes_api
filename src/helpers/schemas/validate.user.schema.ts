42c14119a6813aaf3317c526cfbb7405cb1275ca
Luis Barufi <luisbarufi@gmail.com>
2024-04-19 23:29:00 -0300

    Fixes validation for the user

--- src/helpers/schemas/validate.user.schema.ts
+++ src/helpers/schemas/validate.user.schema.ts
@@ -3,7 +3,7 @@ import { body, param } from 'express-validator';
 export const createUserSchema = [
   body('name').isString().notEmpty().withMessage('Informe o seu nome.'),
   body('email').isEmail().isString().withMessage('E-mail não é válido.'),
-  body('fone').isLength({ max: 15 }).withMessage('Quantidade de caracteres inválida'),
+  body('phone').isLength({ max: 15 }).withMessage('Quantidade de caracteres inválida'),
   body('password').notEmpty().withMessage('Informe a sua senha.'),
   body('password').isLength({ min: 6 }).withMessage('Senha com menos de 6 caracteres.'),
 ];
@@ -12,8 +12,8 @@ export const showUserSchema = [param('id').isUUID().withMessage('O id informado 
 
 export const updateUserSchema = [
   param('id').isUUID().withMessage('O id informado não é valido.'),
-  body('name').isString().notEmpty().withMessage('Informe o seu nome.'),
-  body('email').isEmail().isString().withMessage('E-mail não é válido.'),
+  // body('name').isString().notEmpty().withMessage('Informe o seu nome.'),
+  // body('email').isEmail().isString().withMessage('E-mail não é válido.'),
 ];
 
 export const deleteUserSchema = [param('id').isUUID().withMessage('O id informado não é valido.')];
